af8c02a92b0bcf81f2d8e57d93eb97e0eead8c21
Stefan Ajax <pianoajax@gmail.com>
2020-02-15 10:03:36 +0000

    jaja

--- babymon/models.py
+++ babymon/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+
+class LED(models.Model):
+    on_until = models.DateTimeField()
+    duty_cycle_percent = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
