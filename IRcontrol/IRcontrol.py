025e7aea13aa8704975cf8d69054e97904679f3b
Stefan Ajax <pianoajax@gmail.com>
2020-02-16 22:07:24 +0000

    Duty Cycle Works

--- IRcontrol/IRcontrol.py
+++ IRcontrol/IRcontrol.py
@@ -1,17 +1,34 @@
 from gpiozero import PWMLED
-import sys
 from time import sleep
-
-percent = int(sys.argv[1])
-freq = int(sys.argv[2])
-PWM_value = percent * 0.01
-
+#
+#percent = int(sys.argv[1])
+#freq = int(sys.argv[2])
+#PWM_value = percent * 0.01
+#
 leds = [PWMLED(14), PWMLED(15)]
+#
+#for led in leds:
+#    led.frequency = freq
+#    led.value = PWM_value
 
-for led in leds:
-    led.frequency = freq
-    led.value = PWM_value
+
+import requests
+import json
+
+url = "http://192.168.1.188/babymon/apileds/1/"
+
+payload = {}
+headers = {
+    'Content-Type': 'application/json'
+}
+
 
 while True:
+    response = requests.request("GET", url, headers=headers, data = payload)
+    data = json.loads(response.text.encode('utf8'))
+    on_until = data['on_until']
+    duty_cycle_percent = data['duty_cycle_percent']
+    for led in leds:
+        # led.frequency = freq
+        led.value = duty_cycle_percent*0.01
     sleep(1)
-
